0b4be07231b464734a238a43f92f7365e3b713de
miki-170 <lesiakmikolaj@gmail.com>
2026-09-09 10:36:21 +0200

    set up for the testing

--- src/analyser.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data =pd.read_csv("data/clean/Table_9/combined.csv")
-
-heathrow = data[data['reporting_airport_name'].str.strip(" ")=="HEATHROW"]
-
-ax = heathrow.plot(x='date', y='total_pax_this_period')
-plt.show()
--- src/models/base.py
+++ src/models/base.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.metrics import root_mean_squared_error
 
 data =pd.read_csv("data/clean/Table_9/combined.csv")
+data['date'] = pd.to_datetime(data['date'])
 
-heathrow = data[data['reporting_airport_name'].str.strip(" ")=="HEATHROW"]
 
-ax = heathrow.plot(x='date', y='total_pax_this_period')
-plt.show()
+train_data = data[data['date'].dt.year <2024]
+test_data = data[data['date'].dt.year>=2024]
+
+def model_evaluation(pred, column):
+    return f"Prediction error: {root_mean_squared_error(pred[column],test_data[column])}"
